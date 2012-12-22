86ef1f471dce41b924376267253e7e853d47b481
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2012-12-22 08:02:16 +0700

    Initial commit

---
+++ lib/Perinci/Examples/Bin.pm
@@ -0,0 +1,14 @@
+package Perinci::Examples::Bin;
+
+# VERSION
+
+1;
+# ABSTRACT: Perinci example scripts
+
+=head1 DESCRIPTION
+
+This distribution contains example scripts in bin/. It is split from
+L<Perinci::Example> so the latter does not need to depend on
+L<Perinci::CmdLine>.
+
+=cut
