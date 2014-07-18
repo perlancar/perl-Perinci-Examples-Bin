f95714e3a2163a3641246e91acaebbcaaed6f41c
Steven Haryanto (on PC, Jakarta) <stevenharyanto@gmail.com>
2014-07-18 13:23:28 +0700

    Update Abstract

--- lib/Perinci/Examples/Bin.pm
+++ lib/Perinci/Examples/Bin.pm
@@ -1,17 +1,17 @@
 package Perinci::Examples::Bin;
 
-use 5.010001;
-
 # DATE
 # VERSION
 
+use 5.010001;
+
 1;
-# ABSTRACT: Perinci example scripts
+# ABSTRACT: Perinci example scripts (using Perinci::CmdLine)
 
 =head1 DESCRIPTION
 
-This distribution contains example scripts in bin/. It is split from
-L<Perinci::Examples> so the latter does not need to depend on
-L<Perinci::CmdLine>.
+This distribution contains example scripts in C<bin/> which use
+L<Perinci::CmdLine>. It was split from L<Perinci::Examples> so the latter does
+not need to depend on Perinci::CmdLine.
 
 =cut
