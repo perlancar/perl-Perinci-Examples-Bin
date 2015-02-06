5540c32093fb75c76f55a497a3b1e4cda49388cb
perlancar (@pc-office) <perlancar@gmail.com>
2015-02-06 21:07:36 +0700

    Adjust to Perinci::CmdLine -> Perinci::CmdLine::Classic

---
+++ lib/Perinci/Examples/Bin/Classic.pm
@@ -0,0 +1,17 @@
+package Perinci::Examples::Bin::Classic;
+
+# DATE
+# VERSION
+
+use 5.010001;
+
+1;
+# ABSTRACT: Perinci example CLI scripts (using Perinci::CmdLine::Classic)
+
+=head1 DESCRIPTION
+
+This distribution contains example scripts in C<bin/> which use
+L<Perinci::CmdLine::Classic>. It was split from L<Perinci::Examples> so the
+latter does not need to depend on Perinci::CmdLine::*.
+
+=cut
